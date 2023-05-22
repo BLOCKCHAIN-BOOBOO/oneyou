277c2202710a9763d1f5c377106382834630efd4
UiUxDesignerParamesh <naveennani9100@gmail.com>
2023-05-22 16:52:21 +0530

    update

--- src/components/AppRoutes/AppRoutes.jsx
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -53,45 +53,43 @@ const AppRoutes = () => {
 
   return (
     <div className="App">
-      {/* {userauth?.token && ValidateToken() ?<Menunavbar/>:  <Navbar/>}  */}
-      <Menunavbar />
+      {userauth?.token && ValidateToken() ? <Menunavbar /> : <Navbar />}
+      {/* <Menunavbar /> */}
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/login" element={<Login />}></Route>
 
         <Route path="/signup" element={<Signup />}></Route>
 
-        {userauth?.token && ValidateToken()? (
-        <>
-          <Route path="/profile" element={<Home />}></Route>
+        {userauth?.token && ValidateToken() ? (
+          <>
+            <Route path="/profile" element={<Home />}></Route>
 
-          <Route path="/usersettings" element={<Usersettings />}>
-            <Route path="account" element={<Account />}></Route>
+            <Route path="/usersettings" element={<Usersettings />}>
+              <Route path="account" element={<Account />}></Route>
 
-            <Route
-              path="subscriptionsetting"
-              element={<Subscriptionsetting />}
-            ></Route>
-            <Route
-              path="subscriptions/upgradeplan"
-              element={<Upgradeplan />}
-            ></Route>
-            <Route path="subscriptions/editupi" element={<Editupi />}></Route>
-            <Route
-              path="subscriptions/subscriptionpayment"
-              element={<Subscriptionpayment />}
-            ></Route>
+              <Route
+                path="subscriptionsetting"
+                element={<Subscriptionsetting />}
+              ></Route>
+              <Route
+                path="subscriptions/upgradeplan"
+                element={<Upgradeplan />}
+              ></Route>
+              <Route path="subscriptions/editupi" element={<Editupi />}></Route>
+              <Route
+                path="subscriptions/subscriptionpayment"
+                element={<Subscriptionpayment />}
+              ></Route>
 
-            <Route path="about" element={<Aboutoneyou />}></Route>
-          </Route>
-
-          </>)
-
-           :
-           ( <> 
-          <Route path="/login" element={<Login />}></Route>
-        </>
-         ) } 
+              <Route path="about" element={<Aboutoneyou />}></Route>
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />}></Route>
+          </>
+        )}
 
         <Route path="/*" element={<Login />}></Route>
       </Routes>
