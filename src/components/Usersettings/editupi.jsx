9b7d958b5f48ad40f5e6ddfb740749fd53b6172d
Himani Bondalapati <himanidtpv@gmail.com>
2023-06-26 15:50:43 +0530

    design changes

--- src/components/Usersettings/editupi.jsx
+++ src/components/Usersettings/editupi.jsx
@@ -74,7 +74,7 @@ const Editupi = () => {
                     <td>50</td>
                     <td>Monthly</td>
                     <td className="flex self-center justify-center"><button className="paid-btn py-2 px-7">PAID</button></td>
-                    <td><div className="flex self-center ">Download Invoice <img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
+                    <td><div className="flex self-center "><span className="flex self-center">Download Invoice </span><img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
                 </tr>
                 <tr>
                     <td>UX27032001</td>
@@ -82,7 +82,7 @@ const Editupi = () => {
                     <td>50</td>
                     <td>Monthly</td>
                     <td className="flex self-center justify-center"><button className="paid-btn py-2 px-7">PAID</button></td>
-                    <td><div className="flex self-center">Download Invoice <img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
+                    <td><div className="flex self-center"><span className="flex self-center">Download Invoice</span> <img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
                 </tr>
                 <tr>
                     <td>UX27032001</td>
@@ -90,7 +90,7 @@ const Editupi = () => {
                     <td>50</td>
                     <td>Monthly</td>
                     <td className="flex self-center justify-center"><button className="paid-btn py-2 px-7">PAID</button></td>
-                    <td><div className="flex self-center">Download Invoice <img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
+                    <td><div className="flex self-center"><span className="flex self-center">Download Invoice </span><img src={download} className="flex self-center m-1" width="12" height="12" /></div></td>
                 </tr>
              </tbody>
           
