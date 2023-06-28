712de68d94d6f808598cef904741086ea3794512
Himani Bondalapati <himanidtpv@gmail.com>
2023-06-28 14:36:36 +0530

    design changes

--- src/components/Usersettings/usersettings.jsx
+++ src/components/Usersettings/usersettings.jsx
@@ -44,10 +44,10 @@ const Usersettings = () => {
     <div className="page-background page-body flex self-center align-middle justify-center">
     
      <div className="m-8 flex flex-row w-full h-full settings-page">
-    <div className="flex flex-row w-full h-5/6 mt-16" >      
+    <div className="flex xl:flex-row md:flex-row sm:flex-col flex-col w-full h-5/6 mt-16" >      
       {/* dir="ltr" */}     
         
-        <div className="flex left-0 justify-center h-full w-1/4 m-2">
+        <div className="flex left-0 justify-center h-full xl:w-1/4 md:w-1/4 sm:w-full w-full m-2">
         <div className="home-profile-edit w-full h-full">
             <div className="home-profile-edit-header w-full self-center justify-center flex text-center border-b-2">
               <img src={settings} className="m-2" height="15" width="15" />  User Settings
